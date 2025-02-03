a969f1066d9bdf7b57e8977821ab1f4713402e27
Akshay19203 <119409821+Akshay19203@users.noreply.github.com>
2025-02-03 10:57:38 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Akshay19203/Personal-Portfolio',
   plugins: [react()],
 })
